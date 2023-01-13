392f4f6d00a01cd911d2875b3b4850d29f167a95
Mohsen Mashkour <49885171+MohsenMashkour@users.noreply.github.com>
2023-01-13 21:28:14 +0100

    bottom_sheet.xml added

--- app/src/main/java/com/mkrdeveloper/weatherapp/MainActivity.kt
+++ app/src/main/java/com/mkrdeveloper/weatherapp/MainActivity.kt
@@ -8,17 +8,19 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.mkrdeveloper.weatherapp.adapters.RvAdapter
 import com.mkrdeveloper.weatherapp.databinding.ActivityMainBinding
+import com.mkrdeveloper.weatherapp.databinding.BottomSheetBinding
 import com.mkrdeveloper.weatherapp.forecastModels.Forecast
 import com.mkrdeveloper.weatherapp.forecastModels.WeatherForecastData
 import com.mkrdeveloper.weatherapp.models.WeatherData
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.*
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.awaitResponse
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
@@ -30,13 +32,21 @@ class MainActivity : AppCompatActivity() {
 
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var binding2: BottomSheetBinding
     lateinit var forecastArray : ArrayList<Forecast>
+
+    lateinit var dialog : BottomSheetDialog
 
     //val API : String = "bef83524693fb6717b1bc60c2a919263"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding2 = BottomSheetBinding.inflate(layoutInflater)
+
+        dialog = BottomSheetDialog(this,R.style.BottomSheetTheme)
+
+        dialog.setContentView(binding2.root)
 
         setContentView(binding.root)
 
@@ -45,14 +55,32 @@ class MainActivity : AppCompatActivity() {
 
         forecastArray = arrayListOf()
 
-        binding.rvForecast.setHasFixedSize(true)
-        binding.rvForecast.layoutManager = GridLayoutManager(this,1, RecyclerView.HORIZONTAL,false)
 
 
-        getData()
+        getWeather()
         getForecast()
 
+        binding.imgForecast.setOnClickListener {
+            openForecast()
+        }
+
     }
+
+    @SuppressLint("InflateParams")
+    private fun openForecast() {
+
+
+        //val openDialog = layoutInflater.inflate(R.layout.bottom_sheet,null)
+
+
+        binding2.rvForecast.setHasFixedSize(true)
+        binding2.rvForecast.layoutManager = GridLayoutManager(this,1, RecyclerView.HORIZONTAL,false)
+        dialog.window?.attributes?.windowAnimations = R.style.DialogAnimation
+        dialog.show()
+
+
+    }
+
 
     @OptIn(DelicateCoroutinesApi::class)
     private fun getForecast() {
@@ -86,7 +114,7 @@ class MainActivity : AppCompatActivity() {
 
                 withContext(Dispatchers.Main) {
                  val adapter = RvAdapter(forecastArray)
-                    binding.rvForecast.adapter = adapter
+                    binding2.rvForecast.adapter = adapter
 
                 }
             }
@@ -96,7 +124,7 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("SetTextI18n")
     @OptIn(DelicateCoroutinesApi::class)
-    private fun getData() {
+    private fun getWeather() {
         val api = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
@@ -131,6 +159,12 @@ class MainActivity : AppCompatActivity() {
                         binding.progressBar.visibility = View.GONE
                         binding.subLayout.visibility = View.VISIBLE
 
+                        val iconId = data.weather[0].icon
+
+                        val imageUrl = "https://openweathermap.org/img/w/$iconId.png"
+
+                        Picasso.get().load(imageUrl).into(binding.imgWeather)
+
                         binding.tvSunset.text =
                             SimpleDateFormat(
                                 "hh:mm a",
@@ -143,6 +177,7 @@ class MainActivity : AppCompatActivity() {
                                 Locale.ENGLISH
                             ).format(data.sys.sunrise * 1000)
 
+
                         binding.tvStatus.text = data.weather[0].description
                         binding.tvWind.text = "${data.wind.speed} Km/h"
                         binding.tvLocation.text = "${data.name}\n${data.sys.country}"
@@ -152,11 +187,11 @@ class MainActivity : AppCompatActivity() {
                         binding.tvHumidity.text = "${data.main.humidity}%"
                         binding.tvPressure.text = "${data.main.pressure} hPa"
                         binding.tvUpdateTime.text =
-                            SimpleDateFormat(
+                            "Last update: ${SimpleDateFormat(
                                 "hh:mm a",
                                 Locale.US
                                 // ).format(System.currentTimeMillis())
-                            ).format(data.dt * 1000)
+                            ).format(data.dt * 1000)}"
 
                         Log.d("mkrrrrrrrr", data.dt.toString())
                     }
